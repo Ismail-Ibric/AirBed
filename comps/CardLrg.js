959181cb978ac08d8dcbdf9c689dee38c9682778
Ismail Ibric <iibric@hotmail.com>
2021-10-30 19:24:28 -0400

    Day 3.b Build

--- comps/CardLrg.js
+++ comps/CardLrg.js
@@ -17,7 +17,7 @@ function CardLrg(props) {
       <div className="absolute top-12 left-12 bg-white bg-opacity-30 rounded-lg p-6">
         <h3 className="text-4xl mb-3 w-64">{title}</h3>
         <p>{description}</p>
-        <button className="text-sm text-white bg-red-400 px-4 py-2 rounded-lg mt-5">{buttonText}</button>
+        <button className="text-sm text-white bg-red-400 px-4 py-2 rounded-lg mt-5 hover:shadow-md hover:scale-105 active:scale-100">{buttonText}</button>
       </div>
     </section>
   );
