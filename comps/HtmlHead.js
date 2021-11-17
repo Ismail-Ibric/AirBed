bd532f99788caca4ba2d408ebacc03f0303491f3
Ismail Ibric <iibric@hotmail.com>
2021-11-17 16:28:05 -0500

    added MapBox stylesheet ref

--- comps/HtmlHead.js
+++ comps/HtmlHead.js
@@ -5,6 +5,7 @@ function HtmlHead() {
     <Head>
       <title>AirBed's</title>
       <link rel="icon" href="/favicon.ico" />
+      <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
   )
